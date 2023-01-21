695f70a847b73d6fd17fa47345d80945cffe6c39
PawelGajo <gpawel1997@gmail.com>
2023-01-21 11:37:21 +0100

    remove unused imports

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,13 +3,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { PostCategoryModule } from './post-category/post-category.module';
-import { Post } from './post/entities/post.entity';
-import { PostCategory } from './post-category/entities/post-category.entity';
-import { Answer } from './post/entities/answer.entity';
 
 @Module({
   imports: [
