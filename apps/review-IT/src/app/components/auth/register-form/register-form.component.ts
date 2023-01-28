a24b513aae974bac8a9ee6523183cf241fea675e
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-01-28 23:20:00 +0100

    remove oninit

--- apps/review-IT/src/app/components/auth/register-form/register-form.component.ts
+++ apps/review-IT/src/app/components/auth/register-form/register-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
