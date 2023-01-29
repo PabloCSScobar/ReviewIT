752633314f3507d3bd8a1d3e0c94566d39e05fac
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-01-29 17:12:00 +0100

    remove hardcoded token

--- apps/review-IT/src/app/services/user.service.ts
+++ apps/review-IT/src/app/services/user.service.ts
@@ -10,13 +10,7 @@ export class UserService {
   private http = inject(HttpClient);
   private apiUrl = env.apiUrl;
 
-  private TEMPORARY_TOKEN =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlBhd2XFgiIsInN1YiI6MSwiaWF0IjoxNjc0NjgwNjI1LCJleHAiOjE2NzQ2ODA2ODV9.VP3V7HxIZVn9-vyhCuP2_ZSGzv3DNZiWw6d5QtWuwaY';
-
   getMe() {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.TEMPORARY_TOKEN}`,
-    });
-    return this.http.get<User>(`${this.apiUrl}users/me`, { headers });
+    return this.http.get<User>(`${this.apiUrl}users/me`);
   }
 }
