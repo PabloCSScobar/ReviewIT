5c7b79b3fd9802900429c7eaca5b5ac004f1f973
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-01-29 16:32:57 +0100

    move User model to another file

---
+++ apps/review-IT/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export type User = {
+  id: number;
+  reputation: number;
+  username: string;
+  avatarLink: string;
+};
+
+export type Credentials = {
+  username: string;
+  password: string;
+}
