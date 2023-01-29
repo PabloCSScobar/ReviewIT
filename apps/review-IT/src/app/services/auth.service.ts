5bba375f7e2c1166c9e2a31768130f97709fae82
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-01-29 16:35:40 +0100

    add auth service

---
+++ apps/review-IT/src/app/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { environment as env } from '../../environments/environment';
+import { Credentials, User } from '../models/user';
+
+export interface Token {
+  access_token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  API_URL = env.apiUrl;
+  currentUser$ =new BehaviorSubject<User | null>(null);
+
+  login(credentials: Credentials) {
+    this.http.post<Token>(`${this.API_URL}/auth/login`, credentials)
+    .pipe(
+      tap(token => this.setToken(token.access_token)),
+      ).subscribe(
+      {
+        complete: () =>this.router.navigate(['posts'])
+      }
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('Token');
+    this.router.navigate(['login']);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('Token', token);
+  }
+
+  isLogged() {
+    return !!this.getToken();
+  }
+
+  getToken() {
+    return localStorage.getItem('Token');
+  }
+}
