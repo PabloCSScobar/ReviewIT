2911f6e6a9694979e0d87cd2a487a0d27dd07fa8
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-01-30 16:26:23 +0100

    add iconRegistry to root component

--- apps/review-IT/src/app/app.component.ts
+++ apps/review-IT/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { GITHUB_ICON } from '../assets/icons';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +13,14 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
 })
 export class AppComponent {
-  title = 'review-IT';
+    constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      'github',
+      sanitizer.bypassSecurityTrustHtml(GITHUB_ICON)
+    );
+  }
+
 }
