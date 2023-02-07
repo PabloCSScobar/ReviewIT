d3fa93456c80d111a124a1b291d7e63a37684f73
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-02-07 09:45:07 +0100

    add loadPostDetail effect

--- apps/review-IT/src/app/data-access/effects/post.effects.ts
+++ apps/review-IT/src/app/data-access/effects/post.effects.ts
@@ -3,8 +3,10 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { map, switchMap } from 'rxjs/operators';
 import { PostService } from '../services/post.service';
 import { Post } from '../../models/post';
-import { PostActionTypes } from '../actions/post.actions';
+import { LoadPostDetail, LoadPostDetailSuccess, PostActionTypes } from '../actions/post.actions';
 import { LoadPosts, LoadPostsSuccess } from '../actions/post.actions';
+import { forkJoin } from 'rxjs';
+import { AnswerService } from '../services/answer.service';
 
 
 @Injectable()
@@ -16,8 +18,16 @@ export class PostEffects {
         map((posts: Post[]) => new LoadPostsSuccess(posts))
     ));
 
+    loadPostDetail$ = createEffect(() => this.actions$.pipe(
+        ofType<LoadPostDetail>(PostActionTypes.LoadPostDetail),
+        map(action => action.payload),
+        switchMap((id) => forkJoin([this.postService.getPostDetails(id), this.answerService.getAnswers(id)])),
+        map(([post, answers]) => new LoadPostDetailSuccess(post, answers))
+    ));
+
     constructor(
         private actions$: Actions,
-        private postService: PostService
+        private postService: PostService,
+        private answerService: AnswerService
     ) {}
 }
