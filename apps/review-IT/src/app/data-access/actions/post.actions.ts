b73d6ec7882a2bd378299a9d0800ecbff738e29e
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-02-07 11:37:51 +0100

    add actions for add post/answer

--- apps/review-IT/src/app/data-access/actions/post.actions.ts
+++ apps/review-IT/src/app/data-access/actions/post.actions.ts
@@ -1,6 +1,6 @@
 import { Action } from '@ngrx/store';
-import { Answer } from '../../models/answer.model';
-import { Post, PostDetail } from '../../models/post.model';
+import { Answer, AnswerCreate } from '../../models/answer.model';
+import { Post, PostCreate, PostDetail } from '../../models/post.model';
 
 
 export enum PostActionTypes {
@@ -10,6 +10,12 @@ export enum PostActionTypes {
     LoadPostDetail = '[Post] Load Post Detail',
     LoadPostDetailSuccess = '[Post] Load Post Detail Success',
     LoadPostDetailFail = '[Post] Load Post Detail Fail',
+    AddPost = '[Post] Add Post',
+    AddPostSuccess = '[Post] Add Post Success',
+    AddPostFail = '[Post] Add Post Fail',
+    AddAnswer = '[Post] Add Answer',
+    AddAnswerSuccess = '[Post] Add Answer Success',
+    AddAnswerFail = '[Post] Add Answer Fail',
 }
 
 export class LoadPosts implements Action {
@@ -42,4 +48,34 @@ export class LoadPostDetailFail implements Action {
     constructor(public payload: string) {}
 }
 
-export type PostActions = LoadPosts | LoadPostsSuccess | LoadPostsFail | LoadPostDetail | LoadPostDetailSuccess | LoadPostDetailFail;
+export class AddPost implements Action {
+    readonly type = PostActionTypes.AddPost;
+    constructor(public payload: PostCreate) {}
+}
+
+export class AddPostSuccess implements Action {
+    readonly type = PostActionTypes.AddPostSuccess;
+    constructor(public payload: Post) {}
+}
+
+export class AddPostFail implements Action {
+    readonly type = PostActionTypes.AddPostFail;
+    constructor(public payload: string) {}
+}
+
+export class AddAnswer implements Action {
+    readonly type = PostActionTypes.AddAnswer;
+    constructor(public payload: {postId: number, answer: AnswerCreate}) {}
+}
+
+export class AddAnswerSuccess implements Action {
+    readonly type = PostActionTypes.AddAnswerSuccess;
+    constructor(public payload: Answer) {}
+}
+
+export class AddAnswerFail implements Action {
+    readonly type = PostActionTypes.AddAnswerFail;
+    constructor(public payload: string) {}
+}
+
+export type PostActions = LoadPosts | LoadPostsSuccess | LoadPostsFail | LoadPostDetail | LoadPostDetailSuccess | LoadPostDetailFail | AddPost | AddPostSuccess | AddPostFail | AddAnswer | AddAnswerSuccess | AddAnswerFail;
