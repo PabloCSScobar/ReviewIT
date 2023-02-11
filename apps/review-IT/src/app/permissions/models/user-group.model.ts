921dc4fa5c613c1053973d2bfcb94720d69c1e20
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-02-11 22:56:07 +0100

    add permision user groups

---
+++ apps/review-IT/src/app/permissions/models/user-group.model.ts
@@ -0,0 +1,8 @@
+export const enum UserGroup {
+    NOT_LOGGED_IN = 'not_logged_in',
+    LOGGED_IN = 'logged_in',
+    POST_AUTHOR = 'post_author',
+    ANSWER_AUTHOR = 'answer_author',
+    BASIC = 'basic',
+    ADMIN = 'admin',
+}
