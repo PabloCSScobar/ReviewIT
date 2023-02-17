7c1595a3461ce639a9c02bcac076ac6d8d983e47
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-02-17 21:52:18 +0100

    fix indent

--- apps/review-IT/src/app/permissions/directives/is-not-logged.directive.ts
+++ apps/review-IT/src/app/permissions/directives/is-not-logged.directive.ts
@@ -2,25 +2,25 @@ import { AfterViewChecked, ChangeDetectorRef, Directive, inject, OnInit, Templat
 import { AuthService } from "../services/auth.service";
 
 @Directive({
-    selector: "[IsNotLogged]",
-    standalone: true
+  selector: "[IsNotLogged]",
+  standalone: true
 })
 export class IsNotLoggedDirective implements OnInit {
-    private templateRef = inject(TemplateRef);
-    private viewContainer = inject(ViewContainerRef);
-    private authService = inject(AuthService);
-    private cd = inject(ChangeDetectorRef);
-    private isDisplayed: boolean = false;
+  private templateRef = inject(TemplateRef);
+  private viewContainer = inject(ViewContainerRef);
+  private authService = inject(AuthService);
+  private cd = inject(ChangeDetectorRef);
+  private isDisplayed: boolean = false;
 
-    ngOnInit(): void {
-        const isUserLogged = this.authService.isLogged();
-        if (!isUserLogged && !this.isDisplayed) {
-          this.isDisplayed = true;
-          this.viewContainer.createEmbeddedView(this.templateRef);
-        } else {
-          this.isDisplayed = false;
-          this.viewContainer.clear();
-        }
-        this.cd.detectChanges();
+  ngOnInit(): void {
+    const isUserLogged = this.authService.isLogged();
+    if (!isUserLogged && !this.isDisplayed) {
+      this.isDisplayed = true;
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.isDisplayed = false;
+      this.viewContainer.clear();
+    }
+    this.cd.detectChanges();
   }
 }
