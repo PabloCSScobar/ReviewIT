d8a413167529c87b13eb10c8c71cedd5f3148d6b
gpawel1997@gmail.com <gpawel1997@gmail.com>
2023-03-01 23:25:40 +0100

    add link to enter posts page without logging

--- apps/review-IT/src/app/core/routes/login-page/login-page.component.ts
+++ apps/review-IT/src/app/core/routes/login-page/login-page.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   template: `<div class="login-page">
     <div class="banner"></div>
     <div class="content-wrapper">
@@ -19,6 +20,10 @@ import { RouterModule } from '@angular/router';
       </div>
       <div class="form-wrapper">
         <router-outlet></router-outlet>
+        <div class="continue-with-no-user">
+            <a class="continue-with-no-user-link" routerLink="/posts">Continue without login <mat-icon class="continue-with-no-user-icon" fontIcon="arrow_forward"></mat-icon></a>
+            
+        </div>
       </div>
     </div>
   </div>`,
@@ -62,8 +67,25 @@ import { RouterModule } from '@angular/router';
         height: 100%;
         width: 100%;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
+      }
+      .continue-with-no-user {
+        min-height: 40px;
+      }
+      .continue-with-no-user-link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+      }
+      .continue-with-no-user-link:hover {
+        border-bottom: 1px solid white;
+      }
+      .continue-with-no-user-icon {
+        color: white;
+        margin-left: 5px;
       }
       @media (max-width: 1024px) {
         .login-page {
@@ -76,8 +98,5 @@ import { RouterModule } from '@angular/router';
     `,
   ],
 })
-export class LoginPageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class LoginPageComponent {
 }
